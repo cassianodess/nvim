e7f59ef464785be239c9323f2e72b663572972fc
Cassiano de Souza <cassianodess@hotmail.com>
2024-09-29 11:33:25 -0300

    add dashboard style CSS TECH SOLUTIONS

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,7 +6,35 @@ return {
   {
     'goolord/alpha-nvim',
     config = function()
-      require('alpha').setup(require('alpha.themes.dashboard').config)
+      local alpha = require 'alpha'
+      local dashboard = require 'alpha.themes.dashboard'
+      alpha.setup(dashboard.config)
+
+      dashboard.section.header.val = {
+
+        [[                                  __]],
+        [[     ██████╗███████╗███████╗    ████████╗███████╗ ██████╗██╗  ██╗]],
+        [[    ██╔════╝██╔════╝██╔════╝    ╚══██╔══╝██╔════╝██╔════╝██║  ██║]],
+        [[    ██║     ███████╗███████╗       ██║   █████╗  ██║     ███████║]],
+        [[    ██║     ╚════██║╚════██║       ██║   ██╔══╝  ██║     ██╔══██║]],
+        [[    ╚██████╗███████║███████║       ██║   ███████╗╚██████╗██║  ██║]],
+        [[     ╚═════╝╚══════╝╚══════╝       ╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝]],
+        [[    ███████╗ ██████╗ ██╗     ██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗███████╗]],
+        [[    ██╔════╝██╔═══██╗██║     ██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝]],
+        [[    ███████╗██║   ██║██║     ██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║███████╗]],
+        [[    ╚════██║██║   ██║██║     ██║   ██║   ██║   ██║██║   ██║██║╚██╗██║╚════██║]],
+        [[    ███████║╚██████╔╝███████╗╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║███████║]],
+        [[    ╚══════╝ ╚═════╝ ╚══════╝ ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝]],
+      }
+
+      dashboard.section.buttons.val = {
+        dashboard.button('<C-p>', '  Find file', ':Telescope find_files <CR>'),
+        dashboard.button('<C-n>', '  New file', ':ene <BAR> startinsert <CR>'),
+        dashboard.button('r', '  Recently used files', ':Telescope oldfiles <CR>'),
+        dashboard.button('<leader>fg', '  Find text', ':Telescope live_grep <CR>'),
+        dashboard.button('c', '  Configuration', ':e ~/.config/nvim/init.lua<CR>'),
+        dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
+      }
     end,
   },
   {
