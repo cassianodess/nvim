b109b7539b5512c32c634c6feff7533f06a04807
Cassiano Santos <cassiano.santos@indicoinnovation.pt>
2024-10-03 13:03:18 -0300

    add auto session

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -141,4 +141,21 @@ return {
     'ThePrimeagen/vim-be-good',
     cmd = { 'VimBeGood' },
   },
+  {
+    'rmagatti/auto-session',
+    config = function()
+      require('auto-session').setup {
+        suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+        session_lens = {
+          buftypes_to_ignore = {},
+          load_on_setup = true,
+          theme_conf = { border = true },
+          previewer = false,
+        },
+        vim.keymap.set('n', '<Leader>ls', require('auto-session.session-lens').search_session, {
+          noremap = true,
+        }),
+      }
+    end,
+  },
 }
