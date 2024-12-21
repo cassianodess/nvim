889ed18b3bdc799af8b344cba040d65e905ebe27
Cassiano Santos <cassiano.santos@indicoinnovation.pt>
2024-12-21 01:28:54 -0300

    WIP

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,14 @@
-
 return {
-    {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = {"lua", "javascript", "java"},
-        indent = {enable = true},
-        highlight = {enable = true},
-
-      })
-    end
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				indent = { enable = true },
+				highlight = { enable = true },
+			})
+		end,
+	},
 }
