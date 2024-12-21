0c17201bbabc371025d649e8d5bb97d7ec8d8e1e
Cassiano Santos <cassiano.santos@indicoinnovation.pt>
2024-12-21 14:13:33 -0300

    WIP

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+      local toggleterm = require 'toggleterm'
+      toggleterm.setup {
+        size = 20,
+        open_mapping = [[<c-j]],
+        hide_number = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persister_size = true,
+        direction = 'float',
+        close_on_exit = true,
+        shell = vim.o.shell,
+        float_opts = {
+          border = 'curved',
+          winblend = 0,
+          highlights = {
+            border = 'Normal',
+            background = 'Normal',
+          },
+        },
+      }
+      -- vim.keymap.set({ 'n', 'v', 'c' }, '<C-j>', ':ToggleTerm <CR>')
+      -- vim.keymap.set('t', '<C-j>', [[<C-\><C-n>]], { noremap = true, silent = true })
+      -- vim.keymap.set('n', '<C-j>', ':ToggleTerm<CR>', { noremap = true, silent = true })
+      vim.keymap.set({ 'n', 'v', 'c' }, '<leader>j', ':ToggleTerm <CR>')
+      vim.keymap.set('t', '<leader>j', [[<C-\><C-n>]], { noremap = true, silent = true })
+    end,
+  },
+}
