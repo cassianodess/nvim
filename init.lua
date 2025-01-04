ecf99546af454c3ab4bbca91067679b796d4ccbb
Cassiano Santos <cassiano.santos@indicoinnovation.pt>
2025-01-04 18:48:16 -0300

    show file path in lualine

--- init.lua
+++ init.lua
@@ -32,9 +32,6 @@ require('lazy').setup({
       require('mini.surround').setup()
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = vim.g.have_nerd_font }
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
     end,
   },
   { import = 'plugins' },
